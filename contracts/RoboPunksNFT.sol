d7ed744ef2981b624759adb422b8a0785f613c38
pumb3r <mishakvochko@ukr.net>
2022-12-18 13:41:19 +0200

    adds constructor

--- contracts/RoboPunksNFT.sol
+++ contracts/RoboPunksNFT.sol
@@ -5,13 +5,23 @@ import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
 
 contract RoboPunksNFT is ERC721, Ownable {
-   uint256 public mintPrice;
-   uint256 public totalSupply;
-   uint256 public maxSupply;
-   uint256 public maxPerWallet;
-   bool public isPublicMintEnabled;
-   string internal baseTokenUri;
-   address payable public withdrawWallet;
-   mapping (address => uint256) public walletMints;
-   
+  uint256 public mintPrice;
+  uint256 public totalSupply;
+  uint256 public maxSupply;
+  uint256 public maxPerWallet;
+  bool public isPublicMintEnabled;
+  string internal baseTokenUri;
+  address payable public withdrawWallet;
+  mapping(address => uint256) public walletMints;
+
+  constructor() payable ERC721('RoboPunks', 'RP') {
+    // RP is symbol
+    mintPrice = 0.02 ether;
+    totalSupply = 0;
+    maxSupply = 1000;
+    maxPerWallet = 3;
+    // set withdraw wallet address
+  }
+
+  function setIsPublicMintEnabled(bool isPublicMintEnabled_) external onlyOwner {}
 }
